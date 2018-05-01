d404c12d15d8d935e101ca049830772d247cd623
Gil Sobol <gi-so@web.de>
2018-05-01 15:52:34 +0300

    Final Heuristic

--- ex1.py
+++ ex1.py
@@ -5,92 +5,10 @@ import math
 
 ids = ["935885178", "203609177"]
 
-def highest_distance_dot(state, pacman_row, pacman_col):
-    all_dots=[]
-    maximum_dist=0
-    min_dist = float('Inf')
-    min_dist_pos = [None]*4           #0:TL 1:TR 2:BL 3:BR
-    min_pac_cor = 0
 
-    corner_corner = {}
-    min_distTL = float('Inf')
-    min_distTR = float('Inf')
-    min_distBL = float('Inf')
-    min_distBR = float('Inf')
-    number_of_rows = len(state)
-    number_of_colums = len(state[0])
-
-    for row in range(len(state)):
-        for col in range(len(state[row])):
-            if state[row][col] in (11,71):
-                all_dots.append((row,col))
-    if len(all_dots)>3:
-        for i in range(len(all_dots)):
-            distTL = manhattan_real_distance(0, 0, all_dots[i][0], all_dots[i][1])
-            distTR = manhattan_real_distance(0, number_of_colums-1, all_dots[i][0], all_dots[i][1])
-            distBL = manhattan_real_distance(number_of_rows-1,0 , all_dots[i][0], all_dots[i][1])
-            distBR = manhattan_real_distance(number_of_rows-1, number_of_colums-1, all_dots[i][0], all_dots[i][1])
-            if min_distTL>distTL:
-                min_distTL = distTL
-                min_dist_pos[0] = all_dots[i]
-            if min_distTR>distTR:
-                min_distTR = distTR
-                min_dist_pos[1] = all_dots[i]
-            if min_distBL>distBL:
-                min_distBL = distBL
-                min_dist_pos[2] = all_dots[i]
-            if min_distBR>distBR:
-                min_distBR = distBR
-                min_dist_pos[3] = all_dots[i]
-
-        for pos1 in min_dist_pos:
-            min_dist = float('Inf')
-            for pos2 in min_dist_pos:
-                if pos1 != pos2:
-                    dist = manhattan_real_distance(pos1[0], pos1[1], pos2[0], pos2[1])
-                    if (pos2,pos1) not in corner_corner.keys():
-                        corner_corner[(pos1,pos2)] = dist
-
-        for pos in min_dist_pos:
-            min_dist = float('Inf')
-            dist = manhattan_real_distance(pos[0], pos[1], pacman_row, pacman_col)
-            if dist<min_dist:
-                min_dist=dist
-
-        min_pac_cor=min_dist
-
-        for i in range(4):
-            min=float('Inf')
-            min_combi = None
-            for key in corner_corner.keys():
-                if corner_corner[key]<min:
-                    min = corner_corner[key]
-                    min_combi = key
-            maximum_dist += corner_corner[key]
-            #print(corner_corner[min_combi])
-            for key in corner_corner.keys():
-                if key==min_combi:
-                    corner_corner[key]= float('Inf')
-        #print(maximum_dist)
-
-
-
-    #if len(all_dots)==3:
-    if len(all_dots)==2:
-        for i in range(len(all_dots)):
-            for j in range(len(all_dots)):
-                dist = manhattan_real_distance(all_dots[i][0], all_dots[i][1], all_dots[j][0], all_dots[j][1])
-                if dist > maximum_dist:
-                    maximum_dist = dist
-    if len(all_dots)<2:
-        return 0
-
-
-    return maximum_dist+(min_pac_cor*2)
 
 
 def manhattan_real_distance(packman_row, packman_col, row, col):
-    #print(packman_row,'-', row,'|',packman_col, '-', col)
     return abs(packman_row - row) + abs(packman_col - col)
 
 
@@ -103,26 +21,19 @@ def closest_dot_distance(state, pacman_row, packman_col):
     for row in range(len(state)):
         for col in range(len(state[row])):
             if state[row][col] in(11,71):
-                #print(row,col)
+
                 dist = manhattan_real_distance(pacman_row, packman_col, row, col)
                 if dist<shortest_dist:
                     shortest_dist=dist
                     shortest_dot=(row,col)
-                #if dist>maximum_dist:
-                 #   maximum_dist = dist
-    for row in range(len(state)):
-        for col in range (len(state[row])):
-            if state[row][col] == 11:
-                dist = manhattan_real_distance(shortest_dot[0], shortest_dot[1], row, col)
-                if dist >maximum_dist:
-                    maximum_dist=dist
 
-    #print(shortest_dist)
-    #print(state)
+
+
+
     if shortest_dist == float('Inf'):
         shortest_dist=0
 
-    return  shortest_dist #+ maximum_dist
+    return  shortest_dist
 
 class PacmanProblem(search.Problem):
 
@@ -144,9 +55,7 @@ class PacmanProblem(search.Problem):
         return counter
 
     """This class implements a spaceship problem"""
-    dot_counter = {}
-    best_dot = None
-    states=[]
+
     def __init__(self, initial):
         """Don't forget to set the goal or implement the goal test
         You should change the initial to your own representation"""
@@ -173,7 +82,7 @@ class PacmanProblem(search.Problem):
             allowed_lst.append("R")
         if state[packman_row][packman_col - 1] in allowed_field:
             allowed_lst.append("L")
-        #print(allowed_lst)
+
         return tuple(allowed_lst)
 
     def manhattan_distance(self, state, ghost_row, ghost_col, packman_row, packman_col):
@@ -325,20 +234,15 @@ class PacmanProblem(search.Problem):
                 if ghost_tox[key]<min_dist_per_tox:
                     min_dist_combi = key
                     min_dist_per_tox=ghost_tox[key]
-            min_dist+=min_dist_per_tox
+            min_dist=min_dist_per_tox
 
-            for key in ghost_tox.keys():
-                if min_dist_combi[0] == key[0]:
-                    ghost_tox[key]=float('Inf')
-                if min_dist_combi[1] == key[1]:
-                    ghost_tox[key]=float('Inf')
+
 
         if ghost_counter < len(toxic):
             return float('Inf')
 
-        #print(min_dist)
-        return min_dist+manhattan_real_distance(pacman_row,pacman_col,min_dist_combi[0][0],min_dist_combi[0][1])
 
+        return min_dist
 
 
 
@@ -351,7 +255,7 @@ class PacmanProblem(search.Problem):
         state can be accessed via node.state)
         and returns a goal distance estimate"""
         pacman_row, packman_col = self.find_row_col(node.state, 66)
-        if pacman_row == None:
+        if pacman_row is None:
             return float('inf')
         elif node.state[pacman_row + 1][packman_col] in (51, 50, 40, 41, 30, 31, 20, 21) or node.state[pacman_row - 1][packman_col] in (51, 50, 40, 41, 30, 31, 20, 21) or node.state[pacman_row][packman_col + 1] in (51, 50, 40, 41, 30, 31, 20, 21) or node.state[pacman_row][packman_col - 1] in (51, 50, 40, 41, 30, 31, 20, 21):
             return float('inf')
@@ -364,11 +268,11 @@ class PacmanProblem(search.Problem):
                         ghost_counter+=1
                     if col == 71:
                         toxic_counter+=1
-            print('Poison:', toxic_counter)
-            print('Ghosts:', ghost_counter)
+
+
             if toxic_counter-ghost_counter>0:
                 return float('inf')
-            if ghost_counter>0:
+            if toxic_counter>0 and ghost_counter==1:
                 ghost = None
                 for row in node.state:
                     for col in row:
@@ -379,67 +283,16 @@ class PacmanProblem(search.Problem):
 
             points_left = self.point_sum(node.state)
 
-            #print(points_left)
-            #if ghost_counter<2:
-            #print(node.state)
-
             closest_dot_dist = closest_dot_distance(node.state,pacman_row, packman_col)
             toxic_dist = self.distance_toxic_ghost(node.state, pacman_row, packman_col)
-            most_far_dots = highest_distance_dot(node.state, pacman_row, packman_col)
-
-            return  points_left*2+closest_dot_dist+toxic_dist*3#+((toxic_counter-1)*10000000)
 
 
+            return  points_left*10+closest_dot_dist+toxic_dist*3+((toxic_counter-1)*10)
 
 
-            closest_dot_dist = 0
-            dist_tox_ghost = 0
-            closest_dot_dist = closest_dot_distance(node.state, pacman_row,packman_col)
-            dist_tox_ghost = self.distance_toxic_ghost(node.state,pacman_row, packman_col)
-            most_far_dots=highest_distance_dot(node.state, pacman_row, packman_col)
-            print(points_left/2)
-            #if (points_left + closest_dot_dist)<dist_tox_ghost:
-             #   return dist_tox_ghost
-            #if (points_left + closest_dot_dist)<farest_dot_dist:
-             #   return farest_dot_dist
-            if points_left/2 in self.dot_counter.keys():
-                self.dot_counter[points_left/2]+=1
-            else:
-                self.dot_counter[points_left/2]=1
-            if self.dot_counter[points_left/2]>20:
-                if self.best_dot == None:
-                    shortest_dist = float('Inf')
-                    for row in range(len(node.state)):
-                        for col in range(len(node.state[row])):
-                            if node.state[row][col] == 11:
-                                dist = manhattan_real_distance(pacman_row, packman_col, row, col)
-                                if dist < shortest_dist:
-                                    shortest_dist = dist
-                                    shortest_dot = (row, col)
-                    self.best_dot=shortest_dot
-                    return manhattan_real_distance(pacman_row, packman_col, self.best_dot[0], self.best_dot[1])
-                elif node.state[self.best_dot[0]][self.best_dot[1]]==11:
-                    return manhattan_real_distance(pacman_row, packman_col, self.best_dot[0], self.best_dot[1])
-                else:
-                    self.dot_counter={}
-                    shortest_dist = float('Inf')
-                    for row in range(len(node.state)):
-                        for col in range(len(node.state[row])):
-                            if node.state[row][col] == 11:
-                                dist = manhattan_real_distance(pacman_row, packman_col, row, col)
-                                if dist < shortest_dist:
-                                    shortest_dist = dist
-                                    shortest_dot = (row, col)
-                    self.best_dot = shortest_dot
-                    return manhattan_real_distance(pacman_row, packman_col, self.best_dot[0], self.best_dot[1])
 
 
-            if points_left>most_far_dots:
-                factor2=points_left
-            else:
-                factor2=most_far_dots
 
-        return (closest_dot_dist + points_left)
 
 
 
